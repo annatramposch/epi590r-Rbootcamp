0bc595c5d9caed2323b47ae45fefac62087e52d1
annatramposch <annatramposch@gmail.com>
2024-08-20 09:40:18 -0400

    moved to R folder

---
+++ R/In Class.R
@@ -0,0 +1,45 @@
+# R Bootcamp Day 1 Practice Coding
+# Programmer: Anna Tramposch
+# Contact: anna.tramposch@emory.edu
+# Github: annatramposch, annatramposch@gmail.com
+# Date Created: Mon 8/19/2014
+# Last Update: Mon 8/19/2014
+
+
+# Changed R color theme
+library(rsthemes)
+
+set_theme_light ("Flat White {rsthemes}")
+set_theme_dark()
+set_theme_favorite
+
+#Toggle themes on and off
+use_theme_light(quietly = FALSE)
+use_theme_dark(quietly = FALSE)
+use_theme_toggle()
+use_theme_auto()
+
+--------------------------------------------------------------------------------------------------------------
+
+
+# Day 1
+
+# What we covered in class
+
+# Table1-examples
+# regression-examples
+
+
+
+--------------------------------------------------------------------------------------------------------------
+# DAY 2
+
+# Quarto
+
+# can make slides and presentations based on R script with code & text interspersed
+
+
+# Quarto (newer) vs R Markdown (older)
+# render vs knit
+
+
